2a175b64fccc4d57a85939d5a572aaeef312990b
Eumir Gonzales <gonzales.eumir@gmail.com>
2023-06-27 23:18:53 +1200

    Add initial login with firebase

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -1,4 +1,7 @@
 import { Component, inject, OnInit } from '@angular/core';
+import { Auth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from '@angular/fire/auth';
+// import { Auth } from '@angular/fire/auth';
+import { FormsModule } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
@@ -7,14 +10,44 @@ import { IonicModule } from '@ionic/angular';
   templateUrl: './folder.page.html',
   styleUrls: ['./folder.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, FormsModule],
 })
 export class FolderPage implements OnInit {
   public folder!: string;
   private activatedRoute = inject(ActivatedRoute);
-  constructor() {}
+
+  email= '';
+  password= '';
+
+  emailDisplay = '';
+
+  constructor(public auth: Auth) {}
 
   ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id') as string;
   }
+
+
+  async register() {
+		try {
+			const user = await createUserWithEmailAndPassword(this.auth, this.email, this.password);
+      console.log(user)
+			return user;
+		} catch (e) {
+      console.log(e)
+			return null;
+		}
+	}
+
+	async login() {
+		try {
+			const user = await signInWithEmailAndPassword(this.auth, this.email, this.password);
+      console.log(user.user.email)
+      this.emailDisplay = user?.user?.email || '';
+			return user;
+		} catch (e) {
+      console.log(e)
+			return null;
+		}
+	}
 }
--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,9 @@ import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
 import { provideServiceWorker } from '@angular/service-worker';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAuth, getAuth, Auth } from '@angular/fire/auth';
+// import { defineCustomElements } from '@ionic/pwa-elements/loader';
 
 if (environment.production) {
   enableProdMode();
@@ -19,7 +22,42 @@ bootstrapApplication(AppComponent, {
     provideRouter(routes),
     provideServiceWorker('ngsw-worker.js', {
         enabled: !isDevMode(),
-        registrationStrategy: 'registerWhenStable:30000'
-    })
+        registrationStrategy: 'registerWhenStable:30000',
+        // ...importProvidersFrom(provideFirebaseApp(() => initializeApp(firebaseConfig))),
+        // ...importProvidersFrom(provideAuth(() => getAuth())),
+        // provideFirebaseApp(() => initializeApp()), 
+
+
+        
+    }),
+    
+    importProvidersFrom(provideFirebaseApp(() => initializeApp(firebaseConfig))),
+    importProvidersFrom(provideAuth(() => getAuth())),
 ],
 });
+
+
+
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDh6UZs4aWx_YJTv2VurCHafeLWcxqD09I",
+  authDomain: "hub3-angular.firebaseapp.com",
+  projectId: "hub3-angular",
+  storageBucket: "hub3-angular.appspot.com",
+  messagingSenderId: "122269265485",
+  appId: "1:122269265485:web:41deaea86f967fbc4cb604",
+  measurementId: "G-TVW98MVP6L"
+};
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+// defineCustomElements(window);
