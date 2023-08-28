a72a825dcf300913a301c665fa3fe27e17926c34
Eumir Gonzales <gonzales.eumir@gmail.com>
2023-08-28 22:39:29 +1200

    Updated raygun setup for ios build

--- src/app/services/app.raygun.setup.ts
+++ src/app/services/app.raygun.setup.ts
@@ -5,11 +5,11 @@ rg4js('apiKey', 'xwFBhlzpBt0CgGDs7nVwvQ');
 rg4js('enableCrashReporting', true);
 rg4js('enablePulse', true);
 rg4js('setUser', {
-  identifier: 'hello@raygun.com', // A unique ID, email address, or another ID such as a UUID
+  identifier: 'iosbuild@raygun.com', // A unique ID, email address, or another ID such as a UUID
   isAnonymous: false, // Indicates whether the user is anonymous or not
-  email: 'hello@raygun.com', // Optional 
-  firstName: 'Ronald', // Optional
-  fullName: 'Ronald Raygun', // Optional
+  email: 'iosbuild@raygun.com', // Optional 
+  firstName: 'ios', // Optional
+  fullName: 'ios build', // Optional
 });
 
 // Create a new ErrorHandler and report an issue straight to Raygun
